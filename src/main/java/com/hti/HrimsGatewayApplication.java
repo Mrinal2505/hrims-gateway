3d9fa52c6965b255f2002ba7e33ba0c911c07152
Mrinal2505 <Mrinal.shrinivas@htinfosystems.com>
2026-06-13 12:46:36 +0530

    Add API Gateway code

---
+++ src/main/java/com/hti/HrimsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrimsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrimsGatewayApplication.class, args);
+    }
+
+}
